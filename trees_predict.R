dc432a199d8929df06cf4bd8c9535d72021b1c26
Maxim Osipov <maksim07@gmail.com>
2013-03-09 12:26:23 +0400

    add error rate

--- trees_predict.R
+++ trees_predict.R
@@ -1,14 +1,18 @@
+# prediction by using trees
+
 library(tree)
 library(BMA)
 
-
+# load data
 load("/tmp/samsungData.rda")
 
+# selects only specified subjects and removes "action" column
 getSubjects <- function(data, subs)
 {
   data[data$subject %in% subs, c(-562)]
 }
 
+# funcion for renaming invalid columns 
 cleanDataStructure <- function(raw)
 {
   colnames(raw) <- gsub("\\(\\)", "_", colnames(raw))
@@ -22,6 +26,7 @@ cleanDataStructure <- function(raw)
   raw
 }
 
+# training with leave one out cross validation
 trainData <- cleanDataStructure(getSubjects(samsungData, c(1, 3, 5, 6)))  
 trainResiduals <- data.frame(prediction = c(), actual = c())
 for (i in 1:nrow(trainData))
@@ -37,6 +42,7 @@ for (i in 1:nrow(trainData))
                " ", trainResiduals[i, "prediction"] == trainResiduals[i, "actual"]))
 }
 
+# calculate errors during cross validation
 trainErrors <- trainResiduals[trainResiduals$prediction != trainResiduals$actual, ]
 trainErrorsTable <- table(trainErrors)
 trainErrorsTable
@@ -44,27 +50,14 @@ trainErrorsTable / sum(trainErrorsTable) >= 0.1
 
 trainData$predicted <- as.integer(trainResiduals$prediction == trainResiduals$actual)
 
-walkSamples <- trainResiduals[trainResiduals$prediction == "walkdown" | trainResiduals$prediction == "walkup",]
-walkSamples <- trainData[trainData$activity == "walkdown" | trainData$activity == "walkup", ]
-walkSamples <- walkSamples[,-c(563)]
 
-walkSamples$activity <- as.integer(walkSamples$activity)
-walkLm <- bic.glm(activity ~ ., data = walkSamples, glm.family="poisson")
-summary(walkLm)
-walkLmStep <- step(walkLm)
-
-
+# testing
 testData <- cleanDataStructure(getSubjects(samsungData, c(27, 28, 29, 30)))
 
 t <- tree(activity ~ ., data=trainData)
 pr <- predict(t, testData, type="class")
 pr <- data.frame(pr = pr, ac = testData$activity)
-head(pr, 300)
 
-sum(pr$pr == pr$ac) / nrow(pr)
-
-
-
-
+sum(pr$pr != pr$ac) / nrow(pr)
 
 
